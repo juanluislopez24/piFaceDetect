4391704ab97933b98aefa1f42f950762c411380c
juanluislopez24 <juanluislopez24@gmail.com>
2018-10-22 19:19:39 -0600

    fixed main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from faceDetector import FaceDetector
 
-if __name__ == "__main__":
-    # execute only if run as a script
-    main()
 
 def main():
     detector = FaceDetector()
     detector.stream()
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
