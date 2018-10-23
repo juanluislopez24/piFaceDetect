ddec8490f9ff76e29336af70a0e136a38a8d946b
juanluislopez24 <juanluislopez24@gmail.com>
2018-10-22 19:43:04 -0500

    face detector

--- multithreading.py
+++ multithreading.py
@@ -10,24 +10,23 @@ import cv2
 
 
 def detect_faces(classifier, inputQueue, outputQueue):
-	# keep looping
-	while True:
-		# check to see if there is a frame in our input queue
-	    if (inputQueue.empty() == False):
-		    frame = inputQueue.get()
-		    frame = cv2.resize(frame, (300, 300))
-		    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
-		    # write the detections to the output queue
+# keep looping
+    while True:
+# check to see if there is a frame in our input queue
+        if (inputQueue.empty() == False):
+            frame = inputQueue.get()
+            #frame = cv2.resize(frame, (300, 300))
+            gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+# write the detections to the output queue
             faces = face_cascade.detectMultiScale(gray, 1.1, 5)
             outputQueue.put(faces)
-            print('hey')
+            #print('hey')
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 inputQueue = Queue(maxsize=1)
 outputQueue = Queue(maxsize=1)
-detections = None
+faces = None
 
 print("[INFO] starting process...")
 p = Process(target=detect_faces, args=(face_cascade, inputQueue,
@@ -41,13 +40,11 @@ vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 fps = FPS().start()
 
+
 while True:
-	# grab the frame from the threaded video stream, resize it, and
-	# grab its imensions
     frame = vs.read()
     frame = imutils.resize(frame, width=400)
     (fH, fW) = frame.shape[:2]
-
     # if the input queue *is* empty, give the current frame to
     # classify
     if inputQueue.empty():
@@ -60,19 +57,22 @@ while True:
     # check to see if our detectios are not None (and if so, we'll
     # draw the detections on the frame)
     if faces is not None:
+        #send to our machine learning service
+        
+        
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
 
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+        # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
 
-	# update the FPS counter
-	fps.update()
+    # update the FPS counter
+    fps.update()
 
 # stop the timer and display FPS information
 fps.stop()
